83aecf23884283e0535af02b7e5efdf7c2beb2f8
Garrett Johnson <garrettrayj@gmail.com>
2016-03-22 23:57:13 -0500

    Removed scope check in ConfigurationBuilder

--- Configuration/ConfigurationBuilder.php
+++ Configuration/ConfigurationBuilder.php
@@ -185,19 +185,17 @@ class ConfigurationBuilder
     {
         $baseUrl = '';
 
-        if ($this->container->isScopeActive('request')) {
-            if ($this->container->getParameter('assetic.use_controller')) {
-                $baseUrl = $this->container->get('request')->getBaseUrl();
-            } else {
-                $baseUrl = $this->container->get('templating.helper.assets')->getUrl('');
+        if ($this->container->getParameter('assetic.use_controller')) {
+            $baseUrl = $this->container->get('request')->getBaseUrl();
+        } else {
+            $baseUrl = $this->container->get('templating.helper.assets')->getUrl('');
 
-                // Remove ?version from the end of the base URL
-                if (($pos = strpos($baseUrl, '?')) !== false) {
-                    $baseUrl = substr($baseUrl, 0, $pos);
-                }
+            // Remove ?version from the end of the base URL
+            if (($pos = strpos($baseUrl, '?')) !== false) {
+                $baseUrl = substr($baseUrl, 0, $pos);
             }
         }
-
+        
         // Remove trailing slash, if there is one
         return rtrim($baseUrl, '/');
     }
